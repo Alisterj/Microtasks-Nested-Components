bb9fea4ed655a88b4a6102017001363ff71e50cc
Alisterj <lisarsiss@gmail.com>
2022-03-10 11:28:38 +0300

    first commit

---
+++ src/site/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+type HeaderPropsType = {
+    title: string
+}
+
+const Header = (props: HeaderPropsType) => {
+    return (
+        <>
+            {props.title}
+        </>
+    );
+};
+
+export default Header;
